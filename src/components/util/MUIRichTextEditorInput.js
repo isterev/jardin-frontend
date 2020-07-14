5b86119b1b7e66be41fe0aac0123b5d3d71ea8fc
abhirathi123 <iamabhirathi@gmail.com>
2020-07-14 22:49:01 +0200

    Jardin blogs with rich text area

---
+++ src/components/util/MUIRichTextEditorInput.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+import MUIRichTextEditor from 'mui-rte';
+import FormHelperText from "@material-ui/core/FormHelperText";
+
+
+const styles = (theme) => ({
+
+});
+
+class MUIRichTextEditorInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onSave = this.onSave.bind(this)
+    }
+
+    onSave(data) {
+        alert(data)
+        this.props.form.setFieldValue(this.props.field.name, data)
+        this.props.form.submit()
+    }
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <React.Fragment>
+
+                <MUIRichTextEditor
+                    label="Type something here..."
+                    inlineToolbar={true}
+                    {...this.props} onSave={this.onSave}/>
+
+                {
+                    this.props.form.touched[this.props.field.name] &&
+                    this.props.form.errors[this.props.field.name] &&
+
+                    <FormHelperText error={true}>
+                        {this.props.form.errors[this.props.field.name]}
+                    </FormHelperText>
+                }
+            </React.Fragment>
+        );
+    }
+}
+
+export default withStyles(styles)(MUIRichTextEditorInput);
