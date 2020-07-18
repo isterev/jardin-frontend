faf8e534a07e8c72b6bda587ac3ef39f16c92cbc
jayanthi <jay.ekappan@tum.de>
2020-07-18 23:31:36 +0200

    changed login and signup to function based components

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -11,10 +11,52 @@ import {Button, Card} from '@material-ui/core'
 import {TextField} from 'formik-material-ui'
 import Page from './Page'
 import FormHelperText from "@material-ui/core/FormHelperText"
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import {createMuiTheme} from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles"
+import green from "@material-ui/core/colors/green";
+import Header from "./util/Header";
+import Footer from "./util/Footer";
 
-
-const style = {maxWidth: 500}
-
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#2D6042'
+        },
+        secondary: {
+            main: '#A6BC29'
+        },
+        background: {
+            default: "#CFE070"
+        }
+    },
+})
+const styles = (theme) => ({
+    paper: {
+        marginTop: 70,
+        //marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        //margin: theme.spacing(1),
+        margin: 100,
+        backgroundColor: green,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        margin: 100,
+        //marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: 100,
+        //margin: theme.spacing(3, 0, 2),
+    },
+});
 
 class UserLogin extends React.Component {
 
@@ -50,67 +92,75 @@ class UserLogin extends React.Component {
     }
 
     render() {
+        const classes = styles();
         return (
-
             <div>
-                <Page>
-                    <Card style={style}>
-                        <Formik
-                            initialValues={{
-                                username: this.state.username,
-                                password: this.state.password
-                            }}
-                            validationSchema={this.getSchema}
-                            onSubmit={this.handleSubmit}
-                            render={() => (
-                                <Form mode='structured'>
-                                    <br/>
+                <Header/>
+                <Card>
+                    <Formik
+                        initialValues={{
+                            username: this.state.username,
+                            password: this.state.password
+                        }}
+                        validationSchema={this.getSchema}
+                        onSubmit={this.handleSubmit}
+                        render={() => (
+                            <form className={classes.form}>
+                                <Field
+                                    type="TextField"
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                />
+                                <Field
+                                    type="TextField"
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary"/>}
+                                    label="Remember me"
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.submit}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="#" variant="body2">
+                                            Forgot password?
+                                        </Link>
+                                    </Grid>
+                                    <Grid item>
+                                        <Link to="/register" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </form>
 
-                                    <Field
-                                        component={TextField}
-                                        name="username"
-                                        label="Username"
-                                    />
-                                    <br/>
-                                    <Field
-                                        component={TextField}
-                                        type="password"
-                                        label="Password"
-                                        name="password"
-                                    />
-
-                                    <Button
-                                        type="submit"
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={(() => this.form.submit())}
-                                    >
-                                        Login
-                                    </Button>
-
-                                    <Button
-                                        type="reset"
-                                        variant="contained"
-                                        color="secondary"
-                                    >
-                                        Dismiss
-                                    </Button>
-
-                                    <Link to={'/register'}>Not registered yet?</Link>
-
-                                    {this.props.error &&
-                                         <FormHelperText error={true}>
-                                             {`${this.props.error}`}
-                                         </FormHelperText>
-                                    }
-
-                                </Form>
-
-                            )}
-                        />
-                    </Card>
-                </Page>
-
+                        )}
+                    />
+                </Card>
+                <Footer/>
             </div>
         )
     }
--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,159 +1,128 @@
-"use strict"
 
-import React from 'react'
-import {withRouter} from 'react-router-dom'
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-import {Field, Form, Formik} from 'formik'
-import * as yup from 'yup'
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-import {Button, Card, FormHelperText} from '@material-ui/core'
+export default function SignUp() {
+    const classes = useStyles();
 
-import {TextField} from 'formik-material-ui'
-import Page from './Page'
-
-
-const style = {maxWidth: 500, backgroundColor: "#cede6e"}
-
-
-class UserSignUp extends React.Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            username: '',
-            password: '',
-            email: '',
-            firstName: '',
-            lastName: ''
-        }
-
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleSubmit(values) {
-
-        let user = {
-            username: values.username,
-            password: values.password,
-            email: values.email,
-            firstName: values.firstName,
-            lastName: values.lastName
-        }
-
-        this.props.onSubmit(user)
-    }
-
-    // validation with yup
-    getSchema() {
-        return yup.object().shape({
-            username: yup.string()
-                .required('Username is required'),
-            password: yup.string()
-                .required('Password is required'),
-            email: yup.string()
-                .required('Email is required'),
-            firstName: yup.string()
-                .required('First name is required'),
-            lastName: yup.string()
-                .required('Last name is required'),
-        })
-    }
-
-    render() {
-        return (
-
-            <div>
-                <Page>
-                    <Card style={style}>
-                        <Formik
-                            initialValues={{
-                                username: this.state.username,
-                                password: this.state.password,
-                                email: this.state.email,
-                                firstName: this.state.firstName,
-                                lastName: this.state.lastName,
-
-                            }}
-                            validationSchema={this.getSchema}
-                            onSubmit={this.handleSubmit}
-                            render={() => (
-                                <Form mode='structured'>
-                                    <br/>
-
-                                    <Field
-                                        component={TextField}
-                                        name="username"
-                                        label="Username"
-                                    />
-                                    <br/>
-                                    <Field
-                                        component={TextField}
-                                        type="password"
-                                        label="Password"
-                                        name="password"
-                                    />
-                                    <br/>
-                                    <Field
-                                        component={TextField}
-                                        name="email"
-                                        label="User Email"
-                                        type="email"
-                                    />
-                                    <br/>
-                                    <Field
-                                        component={TextField}
-                                        name="firstName"
-                                        label="First Name"
-                                    />
-                                    <br/>
-
-                                    <Field
-                                        component={TextField}
-                                        name="lastName"
-                                        label="Last Name"
-                                    />
-
-                                    <br></br>
-
-
-                                    <Button
-                                        type="submit"
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={(() => this.form.submit())}
-                                    >
-                                        Register
-                                    </Button>
-
-
-                                    <Button
-                                        type="reset"
-                                        variant="contained"
-                                        color="secondary"
-                                    >
-                                        Dismiss
-                                    </Button>
-
-                                    {this.props.error &&
-                                         <FormHelperText error={true}>
-                                             {`${this.props.error}`}
-                                         </FormHelperText>
-                                    }
-
-                                </Form>
-
-
-                            )}
-                        />
-                    </Card>
-                </Page>
-
+    return (
+        <Page>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="fname"
+                                name="firstName"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                autoComplete="lname"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={(() => this.form.submit())}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
             </div>
-        )
-    }
-
-
+        </Container>
+        </Page>
+    );
 }
-
-export default withRouter(UserSignUp)
